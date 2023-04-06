5d005c7015ee2497006b9719f7bd1565fa8501dd
Diego Dorrejo <dorrejo14@gmail.com>
2023-04-06 14:07:43 -0400

    first commit, created first components with semantic-ui

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,20 @@
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar";
+import { useState } from "react";
+
+
+function App() {
+  const [formOpen, setFormOpen] = useState(false)
+
+  return (
+    <>
+      <NavBar setFormOpen={setFormOpen} />
+      <Container className="main">
+        <EventDashboard setFormOpen={setFormOpen} formOpen={formOpen} />
+      </Container>
+    </>
+  );
+}
+
+export default App;
